be757a82922e899013b84190fe8d77d6266bac11
Clivern <hello@clivern.com>
2022-04-06 17:57:30 +0200

    update

---
+++ lib/chat_web/controllers/user_controller.ex
@@ -0,0 +1,7 @@
+# Copyright 2022 Clivern. All rights reserved.
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file.
+
+defmodule ChatWeb.UserController do
+  use ChatWeb, :controller
+end
