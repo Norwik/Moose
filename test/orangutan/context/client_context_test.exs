dffe46d86b5a893d0bf74c22881c015f237509c0
Clivern <hello@clivern.com>
2022-08-07 02:06:01 +0200

    [test] update

--- test/orangutan/context/client_context_test.exs
+++ test/orangutan/context/client_context_test.exs
@@ -7,4 +7,21 @@ defmodule Orangutan.Context.ClientContextTest do
   Client Context Test Cases
   """
   use Orangutan.DataCase
+  alias Orangutan.Context.ClientContext, as: ClientContext
+
+  describe "client_context" do
+    test "new_client/1 test cases" do
+      client =
+        ClientContext.new_client(%{
+          age: 30,
+          country: "netherlands",
+          gender: "male",
+          state: "amsterdam",
+          username: "admin",
+          user_id: nil
+        })
+
+      assert client.age == 30
+    end
+  end
 end
